21866258d2578f5c139f46a9bf70cc27dcd865b1
Pancho Pico <panchopico@pm.me>
2019-09-18 01:20:46 -0500

    Added motion to date/time display

--- oled_clock.py
+++ oled_clock.py
@@ -46,33 +46,47 @@ xl1 = 0
 xl2 = 10
 
 # Load nice silkscreen font
-font = ImageFont.truetype('/home/pi/fonts/slkscr.ttf', 16)
+font = ImageFont.truetype('/home/pi/fonts/slkscr.ttf', 12)
 
 def clrdisp():
        disp.fill(0)
        disp.show()
 
-while (time.time() - start_time) <= 10:
+while (time.time() - start_time) <= 30:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
     timestr = time.asctime().split(' ')
     line1 = f'{timestr[0]}, {timestr[1]} {timestr[2]}'
     line2 = f'{timestr[3]}'
-    
+    l1fx = font.getsize(line1)[0]
+    l2fx = font.getsize(line2)[0]
+    l2fy = font.getsize(line2)[1]
     draw.text((xl1, top), line1, font=font, fill=255)
-    draw.text((xl2, top+15), line2, font=font, fill=255)
+    draw.text((xl2, top+12), line2, font=font, fill=255)
 
     # Display image.
     disp.image(image)
     disp.show()
-    #xl1 += 1
-    #xl2 += 2
+
+    if xl1 + l1fx >= 130:
+           xl1 = 0
+    else: xl1 += 1
+
+    if xl2 + l2fx >= 130:
+           xl2 = 0
+    else: xl2 += 2
+
+    if (top + 15 + l2fy) >= 32:
+           top = -2
+    else: top += 1
+    
     time.sleep(.1)
 
 print("All done")
 draw.rectangle((0, 0, width, height), outline=0, fill=0)
-draw.text((0,0), "ALL DONE!", font = font, fill=255)
+fontz = ImageFont.truetype('/home/pi/fonts/slkscr.ttf', 22)
+draw.text((0,0), "ALL DONE!", font = fontz, fill=255)
 disp.image(image)
 disp.show()
 time.sleep(3)
